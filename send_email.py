21e4c620c0cf1d547eaa52ed107e8dc51184b4ed
Marlon Rodriguez Rivera <marlon7piri@gmail.com>
2025-07-17 09:49:57 -0500

    first commit 2

--- send_email.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-EMAIL_USER = "marlon7piri@gmail.com"
-EMAIL_PASS = "ywyu pgex nuex gwqs"
-
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.set_debuglevel(1)
-    smtp.login(EMAIL_USER, EMAIL_PASS)
-    print("Login OK")
